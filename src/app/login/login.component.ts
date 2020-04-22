4e2246a4cff619ee09710375905d81a64020e518
latuan <lathanhtuan@outlook.com>
2020-04-22 19:49:04 +0700

    Login/Logout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NotificationService } from '../core/services/notification.service';
+import { AuthenService } from '../core/services/authen.service';
+import { MessageContstants } from '../core/common/message.constants';
+import { UrlConstants } from '../core/common/url.constants';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  model: any;
+  returnUrl: string;
+  constructor(private authenService: AuthenService,
+    private notificationService: NotificationService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
   }
-
+  login() {
+    this.loading = true;
+    this.authenService.login(this.model.userName, this.model.password).subscribe(data => {
+      this.router.navigate([UrlConstants.HOME])
+    }, error => {
+      this.notificationService.printErrorMessage(MessageContstants.SYSTEM_ERROR_MSG);
+      this.loading = false;
+    })
+  }
 }
